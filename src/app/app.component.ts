b549fa45a0117733a89a01095ca9f97517ca6e8d
Timeshift72 <dossantosfilipe7@gmail.com>
2022-07-20 15:41:24 +0200

    methode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import {
+  Component,OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template:'<h1>Welcome to {{title}}!</h1>'
+  template:'<h1>Welcome to {{pokemonList[0]}}!</h1>'
 })
-export class AppComponent {
-  title = 'ng-pokemon-app';
+export class AppComponent implements OnInit {
+  title = 'application de pokémons';
+  pokemonList = ['Bulbizarre', 'Salamèche', 'Carapuce'];
+
+  ngOnInit(){
+    console.table(this.pokemonList)
+    this.selectPokemon('Bulbizarre');
+  }
+
+  selectPokemon(pokemonName: string) {
+    console.log(`Vous avez cliqué sur le pokémon ${pokemonName}`);
+  }
 }
