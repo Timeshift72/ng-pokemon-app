3f6ce2a330c5b6c5a85db29896bb238969c946f6
Timeshift72 <dossantosfilipe7@gmail.com>
2022-07-23 17:36:29 +0200

    directive attribut améliorée

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -5,27 +5,32 @@ import { Directive, ElementRef, HostListener , Input } from '@angular/core';
 })
 export class BorderCardDirective {
 
+  private initialColor: string = '#f5f5f5';
+  private defaultColor: string = '#009688';
+  private defaultHeight: number = 180;
+
   constructor(private el: ElementRef) { 
-    this.setHeight(180);
-    this.setBorder('#f5f5f5')
+    this.setHeight(this.defaultHeight);
+    this.setBorder(this.initialColor)
   }
 
   @Input('pkmnBorderCard') borderColor: string;
   @Input() pkmnBorderCard: string;
 
   @HostListener('mouseenter') onMouseEnter() {
-    this.setBorder(this.borderColor ||'#009688');
+    this.setBorder(this.borderColor || this.defaultColor);
   }
 
   @HostListener('mouseleave') onMouseLeave() {
-    this.setBorder('#f5f5f5');
+    this.setBorder(this.initialColor);
   }
 
-  setHeight(height: number) {
-    this.el.nativeElement.style.height = `${height}px`;
+  private setHeight(height: number) {
+    this.el.nativeElement.style.height = height + 'px';
   }
-  setBorder(color: string) {
-    this.el.nativeElement.style.border = `solid 4px ${color}`;
+  private setBorder(color: string) {
+    let border = 'solid 4px' + color;
+    this.el.nativeElement.style.border =  border;
   }
 
 }
