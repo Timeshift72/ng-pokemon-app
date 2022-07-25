3941e28bea4e643b28af154052b07b368cd7a759
Timeshift72 <dossantosfilipe7@gmail.com>
2022-07-25 17:40:12 +0200

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { POKEMONS } from './mock-pokemons-list';
-import { Pokemon } from './pokemon';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent implements OnInit {
-  // title = 'application de pokémons';
-  pokemonList: Pokemon[] = POKEMONS;
-  pokemonSelected: Pokemon|undefined;
-
-  ngOnInit() {
-    console.table(this.pokemonList);
-  }
-
-  selectPokemon(pokemonId: string) {
-    const pokemon: Pokemon | undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
-    if (pokemon) {
-      console.log(`Vous avez demandé le pokémon ${pokemon.name}`);
-      this.pokemonSelected = pokemon;
-    } else {
-      console.log(`Vous avez demandé un pokémon qui n'existe pas.`);
-      this.pokemonSelected = pokemon;
-    }
-    
-  }
+export class AppComponent {
 }
